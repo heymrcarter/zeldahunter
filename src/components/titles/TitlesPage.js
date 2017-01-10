685330c14aefa076fb22a77a309ec3cf52bcecbd
Tom Carter <heymrcarter14@gmail.com>
2017-01-09 22:12:00 -0500

    adds titles page grid of logos

--- src/components/titles/TitlesPage.js
+++ src/components/titles/TitlesPage.js
@@ -1,19 +1,43 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import TitleList from './TitleList';
 import { bindActionCreators } from 'redux';
 import * as titleActions from '../../actions/title-actions';
+import { Link } from 'react-router';
 
 class TitlesPage extends Component {
   constructor(props, context) {
     super(props, context);
+
+    this.renderTitles = this.renderTitles.bind(this);
+  }
+
+  renderTitles() {
+    return this.props.titles.map((title, key) => {
+      const url = `/titles/${title.id}`;
+      return (
+        <div className="col-sm-12 col-md-6 col-lg-3 m3-bottom" key={key}>
+          <Link to={url}>
+            <img src={title.logo} alt={title.name} className="img-responsive"/>
+
+            <span className="title-name">{title.name}</span>
+            <span className="sr-only">{this.name}</span>
+          </Link>
+        </div>
+      );
+    });
   }
 
   render() {
     return (
       <div>
         <h1>Zelda Titles</h1>
-        <TitleList titles={this.props.titles} />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="title-list">
+              {this.renderTitles()}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
